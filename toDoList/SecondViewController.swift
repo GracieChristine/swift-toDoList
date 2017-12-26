6417e3051905a5e6b8bb273b58a44ba484506d26
GracieChristine <graciechristineli@gmail.com>
2017-12-26 14:03:37 -0700

    Creating xcode project.

---
+++ toDoList/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  toDoList
+//
+//  Created by Gracie Christine on 12/26/17.
+//  Copyright © 2017 Gracie Christine. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
