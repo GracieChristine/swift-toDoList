198c2e3d6cad2adb5e0f776f54203a7070bb4091
GracieChristine <graciechristineli@gmail.com>
2017-12-30 13:17:25 -0700

    Removing all layout to redesign.

--- toDoList/FirstViewController.swift
+++ toDoList/FirstViewController.swift
@@ -8,34 +8,14 @@
 
 import UIKit
 
-var list = ["Buy milk", "Run 5 miles", "Get Peter", "Play with Billi", "Plant my new plants"];
-
-class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FirstViewController: UIViewController, {
     
     
-    @IBOutlet weak var myTableView: UITableView!
-    
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return (list.count)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
     }
     
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
-        cell.textLabel?.text = list[indexPath.row]
-        
-        return(cell);
-    }
-    
-    public func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == UITableViewCellEditingStyle.delete {
-            list.remove(at: indexPath.row);
-            myTableView.reloadData();
-        }
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        myTableView.reloadData();
-    }
+ 
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- toDoList/SecondViewController.swift
+++ toDoList/SecondViewController.swift
@@ -10,15 +10,7 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
-    @IBOutlet weak var input: UITextField!
     
-    @IBAction func addItem(_ sender: AnyObject) {
-        
-        if (input.text != "") {
-            list.append(input.text!);
-            input.text = "";
-        }
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
